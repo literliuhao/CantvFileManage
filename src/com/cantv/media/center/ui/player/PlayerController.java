22e44924e10139a40a7413a30623dc6a34b1dba5
syl283843350 <283843350@.qq.com>
2016-07-21 14:36:41 +0800

    处理冲突

--- src/com/cantv/media/center/ui/player/PlayerController.java
+++ src/com/cantv/media/center/ui/player/PlayerController.java
@@ -396,40 +396,4 @@ public class PlayerController extends RelativeLayout {
 		}
 	}
 
-<<<<<<< HEAD
-//	static class SRT extends HandlerThread {
-//
-//		private Context mContext;
-//		private PlayerCtrlBarContext mCtrlBarContext;
-//
-//		public SRT(String name, int priority, Context mContext, PlayerCtrlBarContext mCtrlBarContext) {
-//			super(name, priority);
-//			this.mContext = mContext;
-//			this.mCtrlBarContext = mCtrlBarContext;
-//		}
-//
-//		@Override
-//		public void run() {
-//			((VideoPlayActicity) mContext).setSrts(mCtrlBarContext.getPlayerCurPosition());
-//		}
-//	}
-=======
-	static class SRT extends HandlerThread {
-
-		private Context mContext;
-		private PlayerCtrlBarContext mCtrlBarContext;
-
-		public SRT(String name, int priority, Context mContext, PlayerCtrlBarContext mCtrlBarContext) {
-			super(name, priority);
-			this.mContext = mContext;
-			this.mCtrlBarContext = mCtrlBarContext;
-		}
-
-		@Override
-		public void run() {
-			((VideoPlayActicity) mContext).setSrts(mCtrlBarContext.getPlayerCurPosition());
-		}
-	}
->>>>>>> f2c01a6... 调整界面
-
 }
