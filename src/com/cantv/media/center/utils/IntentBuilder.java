4f67f7295f7a53de9b9c2fd26e559699a6c8d86a
liuhao <liuhaoliter@gmail.com>
2016-07-25 11:34:35 +0800

    sync code

--- src/com/cantv/media/center/utils/IntentBuilder.java
+++ src/com/cantv/media/center/utils/IntentBuilder.java
@@ -6,6 +6,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.text.TextUtils;
+import android.widget.Toast;
 
 import com.cantv.media.center.data.Media;
 
@@ -52,11 +53,15 @@ public class IntentBuilder {
                             selectType = "image/*";
                             break;
                     }
-                    Intent intent = new Intent();
-                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    intent.setAction(android.content.Intent.ACTION_VIEW);
-                    intent.setDataAndType(Uri.fromFile(new File(filePath)), selectType);
-                    context.startActivity(intent);
+                    try {
+                    	Intent intent = new Intent();
+                    	intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    	intent.setAction(android.content.Intent.ACTION_VIEW);
+                    	intent.setDataAndType(Uri.fromFile(new File(filePath)), selectType);
+                    	context.startActivity(intent);
+					} catch (Exception e) {
+						Toast.makeText(context, "系统不支持该格式文件", Toast.LENGTH_LONG).show();
+					}
                 }
             });
             dialogBuilder.show();
