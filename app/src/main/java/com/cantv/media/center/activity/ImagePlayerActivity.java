6395778b3106add5ad843c4d23cc811ffb21bc8d
liuhao <liuhaoliter@gmail.com>
2016-08-03 18:38:22 +0800

    大量删减无用图片及相关xml代码文件，替换每一张大图文件，切边、切块、做9.png文件优化性能

--- app/src/main/java/com/cantv/media/center/activity/ImagePlayerActivity.java
+++ app/src/main/java/com/cantv/media/center/activity/ImagePlayerActivity.java
@@ -15,7 +15,6 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.PowerManager;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
