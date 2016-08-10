66b94bcbb1a1118434af851985037e3f743cf271
liuhao <liuhaoliter@gmail.com>
2016-08-10 17:05:41 +0800

    播放器界面修改：根据新发的播放器相关UI修改;视频播放器切换音轨无效

--- app/src/main/java/com/cantv/media/center/activity/VideoPlayActicity.java
+++ app/src/main/java/com/cantv/media/center/activity/VideoPlayActicity.java
@@ -131,9 +131,6 @@ public class VideoPlayActicity extends BasePlayer implements OnVideoSizeChangedL
 
 	@Override
 	protected void runProgressBar() {
-
-
-
 		if(mMenuDialog != null){
 			MenuItem audioTrackMenuItem = VideoPlayActicity.this.list.get(1);
 			audioTrackMenuItem.setChildren(createAudioTrackList());
