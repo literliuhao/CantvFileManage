2c339a1bc56a29caa3fd6d01972b40ca8156b2aa
yibh <yibhgb@gmail.com>
2016-12-02 11:27:03 +0800

    修复:OS-2386

--- app/src/main/java/com/cantv/liteplayer/core/ProxyPlayer.java
+++ app/src/main/java/com/cantv/liteplayer/core/ProxyPlayer.java
@@ -176,8 +176,8 @@ public class ProxyPlayer {
             public boolean onError(MediaPlayer mp, int what, int extra) {
                 Log.w("异常", "文件播放发生异常!");
                 if (null != mExceptionListener && !mRetryPlaye) {
-                    mExceptionListener.ExceHappen();
                     Toast.makeText(MyApplication.getContext(), "不支持当前文件格式!", Toast.LENGTH_SHORT).show();
+                    mExceptionListener.ExceHappen();
                 } else if (mRetryPlaye) {
                     mRetryPlaye = false;
                     if (null != mExceptionListener) {
