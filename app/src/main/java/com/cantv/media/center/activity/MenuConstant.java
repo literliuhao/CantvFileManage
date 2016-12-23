d5ce47f84ddd06433ec594be84294d3f368d18b5
yibh <yibhgb@gmail.com>
2016-12-23 20:09:09 +0800

    内置字幕排序

--- app/src/main/java/com/cantv/media/center/activity/MenuConstant.java
+++ app/src/main/java/com/cantv/media/center/activity/MenuConstant.java
@@ -5,10 +5,8 @@ public class MenuConstant {
     public static final String SUBMENU_AUDIOTRACKER = "音轨";
 
     public static final String SUBMENU_SUBTITLE = "无";
-    public static final String SUBMENU_INSUBTITLE = "内嵌文件";
-    public static final String SUBMENU_OUTSUBTITLE = "外挂文件";
-    public static final String SUBMENU_INSUB = "内嵌";
-    public static final String SUBMENU_OUTSUB = "外挂";
+    public static final String SUBMENU_INSUBTITLE = "字幕";
+    public static final String SUBMENU_OUTSUBTITLE = "字幕";
 
     public static final String SUBMENU_IMAGESCALE_ORIGINAL = "原始比例";
     public static final String SUBMENU_IMAGESCALE_FULL = "全屏播放";
