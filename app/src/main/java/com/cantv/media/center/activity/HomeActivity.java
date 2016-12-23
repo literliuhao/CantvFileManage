b61bffa871cd612fec434367226f79b395597bdf
yibh <yibhgb@gmail.com>
2016-12-23 20:58:44 +0800

    去除首页型号信息

--- app/src/main/java/com/cantv/media/center/activity/HomeActivity.java
+++ app/src/main/java/com/cantv/media/center/activity/HomeActivity.java
@@ -29,7 +29,6 @@ import com.cantv.media.center.constants.FileCategory;
 import com.cantv.media.center.ui.MediaGridView;
 import com.cantv.media.center.utils.FileUtil;
 import com.cantv.media.center.utils.MediaUtils;
-import com.cantv.media.center.utils.SystemCateUtil;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -224,9 +223,11 @@ public class HomeActivity extends Activity implements OnFocusChangeListener {
         registerReceiver(mReceiver, filter);
         mLocalFreeTV.setText(getString(R.string.str_localdiskfree) + MediaUtils.getInternalFree());
         mLocalTotalTV.setText(getString(R.string.str_localdisktotal) + MediaUtils.getInternalTotal());
-        mVersion.setText(FileUtil.getVersionName(this)+" 产品型号:"+ SystemCateUtil.productModel()
-                +"\n固件版本: "+SystemCateUtil.getSystemVersion()+"\n系统版本:Android "
-                + android.os.Build.VERSION.RELEASE);
+        mVersion.setText(FileUtil.getVersionName(this)
+//                +" 产品型号:"+ SystemCateUtil.productModel()
+//                +"\n固件版本: "+SystemCateUtil.getSystemVersion()+"\n系统版本:Android "
+//                + android.os.Build.VERSION.RELEASE
+        );
         alertDialog = new AlertDialog.Builder(mContext).create();
 //        Intent intentStart = new Intent(this, BootDialogService.class);
 //        intentStart.setAction("com.cantv.service.RECEIVER_START");
