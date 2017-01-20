da9de7c30b70c65463e2a898abc6aaa03032acd6
liuhao <liuhaoliter@gmail.com>
2017-01-20 16:09:51 +0800

    文件管理器应产品新需求，针对安装包功能修改为禁止安装APK

--- app/src/main/java/com/cantv/media/center/ui/directory/MediaGridView.java
+++ app/src/main/java/com/cantv/media/center/ui/directory/MediaGridView.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.AsyncTask;
-import android.provider.Settings;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -26,7 +25,6 @@ import com.cantv.media.center.utils.FileUtil;
 import com.cantv.media.center.utils.MediaUtils;
 import com.cantv.media.center.utils.SharedPreferenceUtil;
 import com.cantv.media.center.utils.StringUtil;
-import com.cantv.media.center.utils.SystemCateUtil;
 import com.cantv.media.center.utils.ToastUtils;
 import com.cantv.media.center.utils.cybergarage.FileServer;
 import com.cantv.media.center.utils.cybergarage.FileServer.OnInitlizedListener;
@@ -125,23 +123,23 @@ public class MediaGridView extends CustomGridView {
                 } else {
                     if (item.mType == SourceType.APP) {
                         //添加APP安装设置弹框(OS1.2)
-                        if (SystemCateUtil.isContainsCurrModel()) {
-                            install_app = Settings.System.getString(mActivity.getContentResolver(), "install_app");
-                            if (null == install_app) {
-                                getApkForbidDialog();
-                            } else if (install_app.equals("1")) {
-                                getDisclaimerDialog(item);
-                            } else if (install_app.equals("0")) {
-                                getSettingDialog();
-                            }
-                        } else {
+//                        if (SystemCateUtil.isContainsCurrModel()) {
+//                            install_app = Settings.System.getString(mActivity.getContentResolver(), "install_app");
+//                            if (null == install_app) {
+//                                getApkForbidDialog();
+//                            } else if (install_app.equals("1")) {
+//                                getDisclaimerDialog(item);
+//                            } else if (install_app.equals("0")) {
+//                                getSettingDialog();
+//                            }
+//                        } else {
                             //添加APP弹框(OS1.1)
                             if (flag) {
                                 getDisclaimerDialog(item);
                             } else {
                                 getApkForbidDialog();
                             }
-                        }
+//                        }
                     } else {
                         mActivity.isStartAc = true;
                         MediaUtils.openMedia(mActivity, item.isSharing ? item.sharePath : item.mUri);
